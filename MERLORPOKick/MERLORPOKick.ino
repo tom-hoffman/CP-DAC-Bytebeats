e9004cab433063eb8c85419d93622068059237e5
Tom Hoffman <tom.hoffman@gmail.com>
2024-09-14 12:26:51 -0400

    DAC pin change.

--- MERLORPOKick/MERLORPOKick.ino
+++ MERLORPOKick/MERLORPOKick.ino
@@ -9,4 +9,4 @@ void loop(){
     for(int freq = 0; freq < 20; freq++){
       for(int i = 0; i < redic * 90; i++){
         delayMicroseconds(freq/(redic^4));
-        analogWrite(A14,i);}}}}
+        analogWrite(A0,i);}}}}
